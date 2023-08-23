51c49dd4f7dcc2b12e0153c816cf92becd5b7c4e
lucianogalhardo <lrgalhardo@hotmail.com>
2023-08-22 21:52:45 -0300

    atualização 2023-08-22

---
+++ 02 - JavaScript com Orientação a Objetos/Exercícios - JavaScript com Orientação a Objetos/Módulo 02 - Estruturas e Variáveis em JavaScript/MD02_CP02_AppCalculadora.js
@@ -0,0 +1,38 @@
+//* App Calculadora
+
+var n1, n2, resultado;
+
+function calculadora(n1, n2, op){
+ 
+    var op = prompt('Informe a Operação:  + , - , * , / ');
+
+    if (op == '+') {
+        calculadora = n1 + n2;
+
+    } else if(op == '-') {
+        calculadora = n1 - n2;
+
+    } else if (op == '*') {
+        calculadora = n1 * n2;
+
+    } else if (op == '/') {
+        calculadora = n1 / n2;
+
+    } else {
+        throw new Error ('opção inválida !');
+        
+    }
+    
+}
+
+try {
+    var n1 = parseFloat(prompt('Informe o primeiro número: '));
+    var n2 = parseFloat(prompt('Informe o segundo número: '));
+    
+            calculadora (n1, n2);
+            console.log(calculadora);
+        
+} catch (error) {
+    console.log(error);
+}
+
