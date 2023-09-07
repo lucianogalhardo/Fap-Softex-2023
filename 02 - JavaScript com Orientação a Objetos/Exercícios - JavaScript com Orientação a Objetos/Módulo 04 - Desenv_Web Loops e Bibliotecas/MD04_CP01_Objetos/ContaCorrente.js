bd3d96545a96c2f49904b6f34d3bdfd9aeb0e0ad
Luciano Galhardo <lrgalhardo@hotmail.com>
2023-09-07 16:45:37 -0300

    ATUALIZAÇÃO GERAL - (2023-09-07-1645

---
+++ 02 - JavaScript com Orientação a Objetos/Exercícios - JavaScript com Orientação a Objetos/Módulo 04 - Desenv_Web Loops e Bibliotecas/MD04_CP01_Objetos/ContaCorrente.js
@@ -0,0 +1,30 @@
+// Criar um objeto chamado "Banco".
+
+// Propriedades:
+// conta, agência, saldo, tipo de conta
+
+// Métodos: 
+// buscar saldo - deve retornar o valor do saldo atual
+// depósito - passar valor como parâmetro e adcioná-lo no final do objeto
+// saque - passar valor como parâmetro e subtraí-lo no saldo final do objeto
+// numero da conta - deve retornar o número da conta
+
+//importação class 
+import { Clientes } from "./Clientes.js";
+
+
+export class ContaCorrente{
+
+    constructor (clienteBanco, contaBanco, agenciaBanco, tipoContaBanco){
+        
+        this.clienteBanco = clienteBanco;
+        this.contaBanco = contaBanco;
+        this.agenciaBanco = agenciaBanco;
+        this.tipoContaBanco = tipoContaBanco;
+        this.saldoContaBanco = 0.00;
+
+    }
+
+}
+
+        
