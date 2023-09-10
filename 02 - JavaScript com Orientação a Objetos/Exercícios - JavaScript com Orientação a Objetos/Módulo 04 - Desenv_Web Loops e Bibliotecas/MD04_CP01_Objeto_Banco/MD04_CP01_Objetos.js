4c6c3f6eaccd1dd713a6421646f807ad6373bcbb
Luciano Galhardo <lrgalhardo@hotmail.com>
2023-09-10 16:40:47 -0300

    Atualização MD04 - 2023-09-10-1640

---
+++ 02 - JavaScript com Orientação a Objetos/Exercícios - JavaScript com Orientação a Objetos/Módulo 04 - Desenv_Web Loops e Bibliotecas/MD04_CP01_Objeto_Banco/MD04_CP01_Objetos.js
@@ -0,0 +1,161 @@
+// Criar um objeto chamado "Banco".
+// Propriedades:  conta, agência, saldo, tipo de conta
+class Banco{
+
+    constructor(numConta, numAgencia, tipoConta, saldoConta){
+        this.numConta = numConta;
+        this.numAgencia = numAgencia;
+        this.tipoConta = tipoConta;
+        this.saldoConta = 0.00;
+    }
+
+    //métodos ou funções associadas ao objeto:
+    mostrarSaldo = function SaldoValor() {
+        console.log(this.saldoConta);
+        
+    }
+
+    // método mostrar numero da conta
+    mostrarConta = function numeroConta() {
+        console.log(this.numConta);
+    
+    }
+}
+
+// Funcao Menu Principal
+function MenuPrincipal() {
+    op = Number(prompt(`
+    Bem vindo ao Menu principal !!!
+    
+    Selecione a opção desejada:
+    (1)- Contas
+    (2)- Movimentação
+    (3)- Consultar Saldo
+    (4)- Finalizar Sistema`));
+
+}
+
+// Funcao Sub Menu Movimentacao
+function SubMenuMovimentacao() {
+    op2 = prompt(`
+    Menu Movimentação !!!
+    
+    Selecione a opção desejada:
+    (D)- Depósitos
+    (S)- Saques
+    (V)- Voltar ao Menu Principal `).toUpperCase();
+
+}
+
+// Funcao Finalizar Sistema
+function FinalizarSistema() {
+    
+    do {
+        sair = prompt("deseja sair do sistema? ").toUpperCase();   
+            if(sair === "S" || sair === "N"){
+                break;
+                
+            }else {
+                console.log('Opção inválida !!');
+            }
+            
+    } while (sair);
+
+}
+
+function Depositos(creditos) {
+    return creditos = parseFloat(prompt(`Valor Depoósito: `)).toFixed(2);
+    alert('Valor depositado com Sucesso !!!');
+    SubMenuMovimentacao();
+    
+}
+
+function Saques(debitos) {
+    return debitos = parseFloat(prompt(`Valor Saque: `)).toFixed(2);
+    alert('Saque realizado com Sucesso !!!');
+    SubMenuMovimentacao();
+}    
+
+
+
+// Métodos: 
+// depósito - passar valor como parâmetro e adcioná-lo no saldo final do objeto
+// saque - passar valor como parâmetro e subtraí-lo no saldo final do objeto
+// numero da conta - deve retornar o número da conta
+// buscar saldo - deve retornar o valor do saldo atual
+
+
+/*
+var listaContas = [];
+
+var cadConta = new Banco (Number(prompt('Informe o numero da conta: ')), 
+Number(prompt('Informe o número da Agênca: ')),
+prompt('Informe tipo da conta: (CC) Conta corrente | (CP) Conta Poupança'),)
+
+listaContas.push(cadConta);
+
+console.log(listaContas);
+//mostrarSaldo(cadConta.saldoConta);
+cadConta.mostrarConta();
+cadConta.mostrarSaldo();
+
+*/
+
+var op = 0;
+var op2= "";
+var sair;
+var valorDepositos = 0;
+var valorSaques = 0;
+var movimentacao = 0;
+
+do {
+
+    MenuPrincipal();
+
+    switch (op) {
+        case 1:
+            alert('vc escolheu a opção 1');
+            
+            break;
+            
+        case 2:
+            
+            SubMenuMovimentacao();
+                do {
+                    if(op2 === "D"){
+                        Depositos();
+                        valorDepositos += creditos;
+
+                    } else if(op2 === "S"){
+                        Saques();
+                        valorSaques += Saques;
+
+                    } else if(op2 === "V"){
+                        MenuPrincipal();
+
+                    } else {
+                        alert('Opção Inválida !!!');
+
+                    }
+
+                } while (op2 !== "V");    
+                
+            
+            
+        case 3:
+            alert('vc selecionou a opção 3');
+            break;
+            
+        case 4:
+            break;
+   
+        default:
+            alert('Opção inválida, tente novamente !!!')
+    
+    }
+
+    FinalizarSistema();
+
+
+    
+} while (sair == "N");
