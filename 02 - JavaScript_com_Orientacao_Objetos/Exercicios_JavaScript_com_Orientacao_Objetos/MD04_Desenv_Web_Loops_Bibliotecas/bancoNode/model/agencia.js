aae89f7762301a5ef017d51d7c1d1bb4e4554141
Luciano Galhardo <lrgalhardo@hotmail.com>
2023-09-20 11:00:23 -0300

    atualização banco node

---
+++ 02 - JavaScript_com_Orientacao_Objetos/Exercicios_JavaScript_com_Orientacao_Objetos/MD04_Desenv_Web_Loops_Bibliotecas/bancoNode/model/agencia.js
@@ -0,0 +1,11 @@
+export class Agencia{
+    
+    constructor(nome, numero, telefone, email, endereco) {
+        this.nome = nome;
+        this.numero = numero;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+
+    }
+}
---
+++ 02 - JavaScript_com_Orientacao_Objetos/Exercicios_JavaScript_com_Orientacao_Objetos/MD04_Desenv_Web_Loops_Bibliotecas/bancoNode/model/tipoTransacao.js
@@ -0,0 +1,6 @@
+export const TIPOTRANS = {
+    debito: "DEBITO",
+    credito: "CREDITO",
+    transferencia: "TRANSFERENCIA",
+    pagamento: "PAGAMENTO",
+}
