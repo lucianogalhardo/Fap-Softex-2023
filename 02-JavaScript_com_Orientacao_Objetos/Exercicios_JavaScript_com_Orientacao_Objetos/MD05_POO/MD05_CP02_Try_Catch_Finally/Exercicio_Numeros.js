8b7d10fddb02c2771916f40b4137c3976392ec9c
Luciano Galhardo <lrgalhardo@hotmail.com>
2023-10-10 17:09:15 -0300

    ATUALIZAÇÃO TRY_CATCH_FINALLY

--- 02-JavaScript_com_Orientacao_Objetos/Exercicios_JavaScript_com_Orientacao_Objetos/MD05_POO/MD05_CP02_Try_Catch_Finally/Exercicio_Numeros.js
+++ 02-JavaScript_com_Orientacao_Objetos/Exercicios_JavaScript_com_Orientacao_Objetos/MD05_POO/MD05_CP02_Try_Catch_Finally/Exercicio_Numeros.js
@@ -1,14 +1,29 @@
-/*
-var num1;
-var num2;
+const prompt = require ('prompt-sync') ({sigint: true});
 
 try {
-    numeros = parseFloat(prompt('Informe o primeiro número: '));
-    if (isNaN(numeros)) {
-
-    
-} catch (error) {
-    
-}
-
-*/
+    // Solicita ao usuário dois números
+    var numero1 = parseFloat(prompt("Informe o primeiro número:"));
+    var numero2 = parseFloat(prompt("Informe o segundo número:"));
+  
+    // Verifica se o segundo número é zero
+    if (isNaN(numero1) || isNaN(numero2)) {
+      throw new Error("Digite números válidos!");
+    }
+  
+    if (numero2 === 0) {
+      throw new Error("Divisão por zero não é permitida!");
+    }
+  
+    // Realiza a divisão
+    var resultado = numero1 / numero2;
+  
+    // Exibe o resultado
+    console.log("O resultado da divisão é: " + resultado);
+  } catch (excecao) {
+    // Captura e trata a exceção
+    console.log("Ocorreu um erro: " + excecao.message);
+  } finally {
+    // Bloco opcional do finally
+    console.log("Este bloco sempre será executado, independentemente de exceções.");
+  }
+  
